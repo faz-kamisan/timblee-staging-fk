36e32f1aa6912982dfd62a7ea1f11969b0736339
Pratibha <pratibhamansinghka@gmail.com>
2016-07-28 17:20:59 +0530

    Specs setup

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,6 @@ Rails.application.routes.draw do
   resources :folders
   resources :site_maps
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  resources :folders
-
   get  'home/dashboard'
   root 'home#dashboard'
 
