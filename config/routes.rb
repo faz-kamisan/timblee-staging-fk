385214a8ded7e6750a3444feb3e2ba3f29842cdb
Gaurav Mahajan <gaurav@vinsol.com>
2016-07-28 21:28:10 +0530

    Change root path to login page

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :folders
 
   get  'home/dashboard'
-  root 'home#dashboard'
-
+  devise_scope :user do
+    root to: "devise/sessions#new"
+  end
 end
