914ee1902b039566f1439416c0415904938a2d64
Gaurav Mahajan <gaurav@vinsol.com>
2016-08-01 13:06:02 +0530

    Add confirmable to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   belongs_to :business
 
