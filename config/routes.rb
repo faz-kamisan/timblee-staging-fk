459dfcdacae98e42a93750bd8065cb56161ee1fc
Gaurav Mahajan <gaurav@vinsol.com>
2016-08-03 13:27:10 +0530

    Expire previous token when user resends confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
       sessions: 'users/sessions',
       registrations: 'users/registrations',
-      passwords: 'users/passwords'
+      passwords: 'users/passwords',
+      confirmations: 'users/confirmations'
     }
 
   resources :users do
