45fb769fd2f3881f03d6f3bbb5b7f0e4a5bd3491
Gaurav Mahajan <gaurav@vinsol.com>
2016-08-05 16:49:31 +0530

    Verify Folder Name ist all-sitemaps.

--- app/assets/javascripts/delete_user.js
+++ app/assets/javascripts/delete_user.js
@@ -1,7 +1,7 @@
 var DeleteUser = function(options) {
   this.delete_user_link = options.delete_user_link;
   this.user_modal_link = options.user_modal_link;
-  this.user_name_div = options.user_name_div;
+  this.user_name_span = options.user_name_span;
 };
 
 DeleteUser.prototype.bindEvents = function() {
@@ -10,7 +10,7 @@ DeleteUser.prototype.bindEvents = function() {
     delete_user_url = $(this).data('url');
     delete_user_name = $(this).data('name');
     _this.delete_user_link.attr('href', delete_user_url);
-    _this.user_name_div.html(delete_user_name);
+    _this.user_name_span.html(delete_user_name);
   })
 };
 
@@ -19,7 +19,7 @@ $(function() {
   var options = {
     delete_user_link : $('#delete-user-link'),
     user_modal_link: $('.delete-user-modal-link'),
-    user_name_div: $('.user-name-to-delete')
+    user_name_span: $('.user-name-to-delete span')
   }
   var delete_user = new DeleteUser(options);
   delete_user.bindEvents();
--- app/models/folder.rb
+++ app/models/folder.rb
@@ -4,7 +4,15 @@ class Folder < ActiveRecord::Base
   validates :name, :business, presence: true
   validates :name, uniqueness: { scope: :business_id, case_sensitive: false }
 
+  before_validation :check_name_is_not_all_site_maps
   strip_fields :name
 
   scope :order_by_lower_name, -> { order('lower(name)') }
+
+  private
+    def check_name_is_not_all_site_maps
+      if self.name.squeeze(' ').downcase == 'all sitemaps'
+        errors.add(:name, "has already been taken")
+      end
+    end
 end
