cc17843231fe19749519d901a18dd0c2581f9317
Pratibha <pratibhamansinghka@gmail.com>
2016-08-05 11:21:50 +0530

    remove business controller

--- app/controllers/businesses_controller.rb
+++
@@ -1,9 +0,0 @@
-class Businesses_Controller < ApplicationController
-  def bulk_invitation
-    #fix - move to model + optimize it. Discuss.(done)
-    emails = params[:email].split(/\s* \s*/)
-    email_categories = InvitationService.invite_users(emails, current_user, params[:custom_message])
-    set_notice(email_categories)
-    redirect_to team_settings_users_path
-  end
-end
