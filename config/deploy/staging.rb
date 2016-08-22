68dc8643d76de5e7d212119aa5962c20ff335710
Gaurav Mahajan <gaurav@vinsol.com>
2016-08-22 18:47:36 +0530

    Change stagin ip to use elastic ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,7 +9,7 @@ set :branch, 'develop'
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-server '52.64.63.239', user: 'deploy', roles: %w{web app db}
+server '52.64.129.18', user: 'deploy', roles: %w{web app db}
 
 
 # server-based syntax
