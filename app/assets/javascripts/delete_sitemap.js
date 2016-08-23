ea0e709a5beea797c944a0f33b3895b51d9192f6
Saiyam Gambhir <saiyam@vinsol.com>
2016-08-23 14:11:47 +0530

    modals for billing screens

--- app/assets/javascripts/delete_sitemap.js
+++ app/assets/javascripts/delete_sitemap.js
@@ -6,14 +6,15 @@ var DeleteSitemap = function(options) {
 
 DeleteSitemap.prototype.bindEvents = function() {
   var _this = this;
-  this.sitemapModalLink.on('click', function() {
-    var delete_sitemap_url = $(this).data('url'),
-    delete_sitemap_name = $(this).data('name');
+  this.sitemapModalLink.on('click', function(event) {
+    var $this = $(this);
+    var delete_sitemap_url = $this.data('url'),
+    delete_sitemap_name = $this.data('name');
     _this.deleteSitemapLink.attr('href', delete_sitemap_url);
     _this.sitemapNameSpan.html(delete_sitemap_name);
-  })
+    $('#delete-sitemap-modal .modal-body .site-map-clone').html($this.closest('.site-card').clone().addClass('cloned'));
+  });
 };
-
 
 $(function() {
   var options = {
