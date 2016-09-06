83998e6760b0a101928d726850aa7d6b6900e1f3
Saiyam Gambhir <saiyam@vinsol.com>
2016-09-06 16:23:20 +0530

    react header design

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,3 +28,12 @@
 //= require settings
 //= require constants
 //= require modal_fix
+
+$(document).ready(function() {
+  $('.toggle-header').on('click', function() {
+    $('.main-header').toggleClass('toggle');
+    $('.react-header').toggleClass('toggle');
+    $('body').toggleClass('toggle');
+    $('.sitemap-left-sidebar').toggleClass('toggle');
+  });
+});
