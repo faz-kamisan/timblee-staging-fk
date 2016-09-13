90154ff53c3dab98e24fd92d0aa56c695fdbcfe5
Saiyam Gambhir <saiyam@vinsol.com>
2016-09-13 16:55:39 +0530

    designs for comment section

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,7 @@ ActiveRecord::Schema.define(version: 20160908063247) do
 
   create_table "guests", force: :cascade do |t|
     t.string   "full_name"
-    t.string   "email"
+    t.string   "logo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -116,7 +116,6 @@ ActiveRecord::Schema.define(version: 20160908063247) do
   create_table "subscriptions", force: :cascade do |t|
     t.integer  "quantity"
     t.integer  "no_of_users"
-    t.integer  "plan_id"
     t.integer  "business_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
@@ -157,7 +156,6 @@ ActiveRecord::Schema.define(version: 20160908063247) do
     t.string   "unconfirmed_email"
     t.boolean  "notify_by_email",        default: true
     t.datetime "deleted_at"
-    t.boolean  "is_super_admin",         default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
