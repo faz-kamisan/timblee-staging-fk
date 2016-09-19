76b228b92b3f87f5a70cb08093de2c9b166a8014
Pratibha <pratibhamansinghka@gmail.com>
2016-09-19 18:06:35 +0530

    logo fixes

--- app/assets/javascripts/personalization.js
+++ app/assets/javascripts/personalization.js
@@ -17,13 +17,30 @@ Personalization.prototype.bindEvents = function() {
     _this.rowSave.removeClass('hide');
     _this.rowEdit.addClass('hide');
     _this.logoField.val('');
+    _this.chooseLogo.attr('src', _this.chooseLogo.data('original-value'))
   });
 
   this.chooseLogo.on('click', function() {
     _this.logoField.click();
   });
+
+  this.logoField.change(function(){
+      _this.readURL(this);
+  });
 };
 
+Personalization.prototype.readURL = function(input) {
+  var _this = this;
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      _this.chooseLogo.attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+};
 
 $(function() {
   var options = {
