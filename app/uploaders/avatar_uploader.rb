460e0f622f147a86f9e943d9e5b7c50c0186ad1c
Pratibha <pratibhamansinghka@gmail.com>
2016-09-20 13:34:37 +0530

    resize avatar

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,5 +1,12 @@
 class AvatarUploader < CarrierWave::Uploader::Base
   storage :file
+  include CarrierWave::MiniMagick
+
+  process resize_to_fit: [100, 100], :if => :is_not_svg?
+
+  def is_not_svg?(picture)
+    true unless picture.content_type == "image/svg+xml"
+  end
 
   def store_dir
     "assets/uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
