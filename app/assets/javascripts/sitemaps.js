42ca924d7fe5f71094fc8020b16333c0bc2f4e06
Gaurav Mahajan <gaurav@vinsol.com>
2016-09-23 20:06:01 +0530

    Fix modal closing

--- app/assets/javascripts/sitemaps.js
+++ app/assets/javascripts/sitemaps.js
@@ -3,6 +3,10 @@ var Sitemaps = function(options) {
   this.newSitemapLink = options.newSitemapLink;
   this.newSitemap = options.newSitemap;
   this.sitemapErrorModal = options.sitemapErrorModal;
+  this.inviteErrorModal = options.inviteErrorModal;
+  this.inviteErrorLink = options.inviteErrorLink;
+  this.deleteSitemapModal = options.deleteSitemapModal;
+  this.deleteSitemapLink = options.deleteSitemapLink
   this.dropContainers = options.dropContainers;
   this.draggableSitemaps = options.draggableSitemaps;
 };
@@ -18,16 +22,24 @@ Sitemaps.prototype.bindEvents = function() {
     }
   });
 
+  $('body').on('click', '.delete-sitemap-modal-link', function(e) {
+    _this.deleteSitemapModal.modal('show')
+  })
+
+  $('body').on('click', '.invite-error-link', function(e) {
+    _this.inviteErrorModal.modal('show')
+  })
+
+  $('.modal').on('hidden.bs.modal', function () {
+    setTimeout(function() {
+    _this.actionOverlays.animate({ top: 230 }, 150);
+    }, 250);
+  });
+
   $('body').on('click', function(event) {
     var $target = $(event.target);
     if($target.closest('.sitemap_wrapper').length > 0) {
       var sitemapId = $target.closest('.sitemap_wrapper').find('.sitemap-container').data('id')
-      _this.actionOverlays.not('[data-id= ' + sitemapId.toString() + ']').animate({ top: 230 }, 150);
-    }
-    else if($target.hasClass('hide-delete-modal')) {
-      setTimeout(function() {
-        _this.actionOverlays.animate({ top: 230 }, 150);
-      }, 250);
     }
     else {
       _this.actionOverlays.animate({ top: 230 }, 150);
@@ -62,6 +74,10 @@ $(function() {
     newSitemap : $('.new_sitemap'),
     newSitemapLink : $('.new-sitemap-link'),
     sitemapErrorModal: $('#sitemap-error-modal'),
+    inviteErrorModal: $('#invite-error-modal'),
+    inviteErrorLink: $('.invite-error-link'),
+    deleteSitemapModal: $('#delete-sitemap-modal'),
+    deleteSitemapLink: $('.delete-sitemap-modal-link'),
     dropContainers : $('.folder-info'),
     draggableSitemaps : $('.sitemap-container').not('.new-sitemap')
   }
