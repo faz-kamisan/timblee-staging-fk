6cf70cba863481b5f2283e3326e3f22207ec5961
Gaurav Mahajan <gaurav@vinsol.com>
2016-10-06 17:14:57 +0530

    Add search ofr sitemap on dashboard

--- app/assets/javascripts/sitemaps.js
+++ app/assets/javascripts/sitemaps.js
@@ -12,6 +12,7 @@ var Sitemaps = function(options) {
   this.copyButtonLink = options.copyButtonLink;
   this.dropContainers = options.dropContainers;
   this.draggableSitemaps = options.draggableSitemaps;
+  this.searchSitemapInput = options.searchSitemapInput;
 };
 
 Sitemaps.prototype.bindEvents = function() {
@@ -22,6 +23,14 @@ Sitemaps.prototype.bindEvents = function() {
       $(this).submit();
     } else {
       _this.sitemapErrorModal.modal('show');
+    }
+  });
+
+  this.searchSitemapInput.on('input', function(e) {
+    $('.sitemap-container').closest('.sitemap-outer-wrapper').removeClass('hide');
+    if(e.target.value.trim().length > 0) {
+      var searchQueryRegExp = new RegExp('^' + e.target.value.trim(), 'i')
+      $('.sitemap-container').not('.new-sitemap').not(function(i, element) { return $(element).data('name').match(searchQueryRegExp) }).closest('.sitemap-outer-wrapper').addClass('hide');
     }
   });
 
@@ -172,7 +181,8 @@ $(function() {
     dropContainers : $('.folder-info'),
     copyButtonLink: $('.copy-link-button'),
     shareMethodTabs: $('.share-method-tabs'),
-    draggableSitemaps : $('.sitemap-container').not('.new-sitemap')
+    draggableSitemaps: $('.sitemap-container').not('.new-sitemap'),
+    searchSitemapInput: $('.search-sitemap')
   }
   var sitemaps = new Sitemaps(options);
   sitemaps.init();
