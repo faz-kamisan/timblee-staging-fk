217b2e7f4cf0432b68fe8fbf7955ebcc271afba2
Pratibha <pratibhamansinghka@gmail.com>
2016-10-06 12:52:46 +0530

    eye fixes

--- app/assets/javascripts/my_info.js
+++ app/assets/javascripts/my_info.js
@@ -1,6 +1,7 @@
 var MyInfo = function(options) {
-  this.btnCancel = options.btnCancel;
-  this.btnEdit = options.btnEdit;
+  this.btnCancelAvatar = options.btnCancelAvatar;
+  this.btnCancelPassword = options.btnCancelPassword;
+  this.btnEditAvatar = options.btnEditAvatar;
   this.rowSave = options.rowSave;
   this.rowEdit = options.rowEdit;
   this.avatarLink = options.avatarLink;
@@ -10,16 +11,17 @@ var MyInfo = function(options) {
   this.eyeClose = options.eyeClose;
   this.eyeOpen = options.eyeOpen;
   this.passwordField = options.passwordField;
+  this.eyesIcons = options.eyesIcons;
 };
 
 MyInfo.prototype.bindEvents = function() {
   var _this = this;
-  this.btnEdit.on('click', function() {
+  this.btnEditAvatar.on('click', function() {
     _this.rowEdit.removeClass('hide');
     _this.rowSave.addClass('hide');
   });
 
-  this.btnCancel.on('click', function() {
+  this.btnCancelAvatar.on('click', function() {
     _this.rowSave.removeClass('hide');
     _this.rowEdit.addClass('hide');
     _this.avatarField.val('');
@@ -27,9 +29,13 @@ MyInfo.prototype.bindEvents = function() {
     _this.previewProfileImage.attr('src', _this.previewProfileImage.data('original-value'))
   });
 
+  this.btnCancelPassword.on('click', function() {
+    _this.eyesIcons.addClass('hide');
+  });
+
   this.avatarField.change(function(){
-      _this.readURL(this);
-      _this.defaultAvatars.attr('checked', false);
+    _this.readURL(this);
+    _this.defaultAvatars.attr('checked', false);
   });
 
   this.avatarLink.on('click', function() {
@@ -54,6 +60,14 @@ MyInfo.prototype.bindEvents = function() {
     _this.eyeOpen.removeClass('hide');
     _this.passwordField.focus();
   });
+
+  this.passwordField.on('input', function(){
+    if($(this).val().length == 0) {
+      _this.eyesIcons.addClass('hide');
+    } else {
+      _this.eyesIcons.removeClass('hide');
+    }
+  })
 };
 
 MyInfo.prototype.readURL = function(input) {
@@ -72,8 +86,9 @@ MyInfo.prototype.readURL = function(input) {
 
 $(function() {
   var options = {
-    btnCancel : $('.btn-cancel-edit-avatar'),
-    btnEdit : $('.btn-edit-avatar'),
+    btnCancelAvatar : $('.btn-cancel-edit-avatar'),
+    btnCancelPassword : $('.btn-cancel-edit-password'),
+    btnEditAvatar : $('.btn-edit-avatar'),
     rowSave : $('.row-avatar-save'),
     rowEdit : $('.row-avatar-edit'),
     avatarLink : $('.avatar-link'),
@@ -82,7 +97,8 @@ $(function() {
     previewProfileImage : $('.preview-profile-image'),
     passwordField : $('input.hidden-password'),
     eyeOpen : $('.icon-eye-open'),
-    eyeClose : $('.icon-eye-close')
+    eyeClose : $('.icon-eye-close'),
+    eyesIcons : $('.eyes-icons')
   }
   new MyInfo(options).bindEvents();
 });
