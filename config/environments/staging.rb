1a2673f5ce1e03f7b434955fd9c1de3af25d6327
Pratibha <pratibhamansinghka@gmail.com>
2016-10-10 13:33:37 +0530

    CM minor fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     server: 'smtp.api.createsend.com',
     port: '587',
+    domain: 'gmail.com',
     user_name: 'f5087f2a-a035-4166-b7e0-7b40c350193a',
     password: 'f5087f2a-a035-4166-b7e0-7b40c350193a'
   }
