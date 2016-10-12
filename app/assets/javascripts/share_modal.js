aaf1990db63bb83c26764a162c7a5dc63096f44d
Gaurav Mahajan <gaurav@vinsol.com>
2016-10-12 13:25:32 +0530

    Fix message add and cancel on share and invite modals.

--- app/assets/javascripts/share_modal.js
+++ app/assets/javascripts/share_modal.js
@@ -12,9 +12,11 @@ var ShareModal = function(options) {
 ShareModal.prototype.bindEvents = function() {
   var _this = this;
   this.addMessageLink.on('click', function() {
-    if(_this.inviteCustomMessage.val().trim().length > 0) {
+    var message = _this.commentInputDiv.find('.twemoji-textarea').html().trim()
+    if(message.length > 0) {
       _this.inviteCustomMessage.data('send', true);
-      _this.messagePreview.find('p:first').html(_this.inviteCustomMessage.val().trim())
+      _this.inviteCustomMessage.val(message);
+      _this.messagePreview.find('p:first').html(message)
       _this.messagePreview.removeClass('hidden')
       _this.commentInputDiv.addClass('hidden')
     }
@@ -22,6 +24,7 @@ ShareModal.prototype.bindEvents = function() {
   this.clearMessageLink.on('click', function() {
     _this.inviteCustomMessage.data('send', false);
     _this.inviteCustomMessage.val('');
+    _this.commentInputDiv.find('.twemoji-textarea').html('');
     _this.messagePreview.find('p:first').html('')
   })
   this.editMessageLink.on('click', function() {
