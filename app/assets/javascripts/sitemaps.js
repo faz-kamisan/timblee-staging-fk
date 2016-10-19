5b044ccab9c4b16e06f935e8acd1ce924fa54925
Gaurav Mahajan <gaurav@vinsol.com>
2016-10-19 19:27:43 +0530

    minor fix

--- app/assets/javascripts/sitemaps.js
+++ app/assets/javascripts/sitemaps.js
@@ -32,12 +32,11 @@ Sitemaps.prototype.bindEvents = function() {
   });
 
   this.searchSitemapInput.on('input', function(e) {
-    debugger
     $('.sitemap-container').closest('.sitemap-outer-wrapper').removeClass('hide');
     if(e.target.value.trim().length > 0) {
       $(e.target).closest('.dashboard-search').addClass('active');
       var searchQueryRegExp = new RegExp(('\\b' + e.target.value.trim()), 'gi')
-      $('.sitemap-container').not('.new-sitemap').not(function(i, element) { return $(element).data('name').match(searchQueryRegExp) }).closest('.sitemap-outer-wrapper').addClass('hide');
+      $('.sitemap-container').not('.new-sitemap').not(function(i, element) { console.log($(element)); console.log($(element).data('name')); return($(element).data('name').match(searchQueryRegExp)) }).closest('.sitemap-outer-wrapper').addClass('hide');
     } else {
       $(e.target).closest('.dashboard-search').removeClass('active');
     }
