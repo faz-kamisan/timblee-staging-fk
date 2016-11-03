6b3efe92ddf2e11d06de5421a677c0e2180ebde9
Bharat Gupta <bindassbharat311@gmail.com>
2016-11-03 17:40:39 +0530

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161103100547) do
+ActiveRecord::Schema.define(version: 20161103120949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,7 +70,7 @@ ActiveRecord::Schema.define(version: 20161103100547) do
     t.string   "recipient_type"
     t.integer  "sitemap_id"
     t.integer  "actor_id"
-    t.integer  "actor_type"
+    t.string   "actor_type"
   end
 
   add_index "notifications", ["sitemap_id"], name: "index_notifications_on_sitemap_id", using: :btree
