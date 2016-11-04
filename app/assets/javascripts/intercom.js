767d088d1ab044fcf7cc91b29ec24e4e5861a956
Pratibha <pratibhamansinghka@gmail.com>
2016-11-04 19:15:51 +0530

    fix intercom

--- app/assets/javascripts/intercom.js
+++ app/assets/javascripts/intercom.js
@@ -1,12 +1,11 @@
-// $(function() {
-//     window.intercomSettings = {
-//       email: $('.get-in-touch').data('user-email') || $('#user_email').val(),
-//       created_at: Date.now(),
-//       app_id: INTERCOM_APP_ID,
-//       custom_launcher_selector: '.get-in-touch'
-//     };
-//     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/'+ INTERCOM_APP_ID;var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
-//   $('.get-in-touch').on('click', function (e) {
-//     $('.intercom-launcher-button').click();
-//   })
-// });
+$(function() {
+  !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
+  analytics.load(SEGMENT_IO_KEY);
+  }}();
+
+  window.intercomSettings = {
+    email: $('.get-in-touch').data('user-email') || $('#user_email').val(),
+    created_at: Date.now(),
+    app_id: INTERCOM_APP_ID,
+    custom_launcher_selector: '.get-in-touch'};
+});
