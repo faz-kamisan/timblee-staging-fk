3b369a36453ceedcd3004966cdde0e5eb54fd669
Pratibha <pratibhamansinghka@gmail.com>
2016-11-08 16:36:22 +0530

    duplicate sitemap functionality fix

--- app/models/sitemap.rb
+++ app/models/sitemap.rb
@@ -43,12 +43,17 @@ class Sitemap < ActiveRecord::Base
     Page.skip_callback(:update, :before, :archive_children_pages)
     duplicate.save
 
-    sections.each { |section| section.duplicate(duplicate) }
+    sections.order(:default).each { |section| section.duplicate(duplicate) }
+    footer_pages.each { |footer_page| footer_page.duplicate(nil, nil, duplicate) }
 
     Sitemap.set_callback(:create, :after, :create_default_section_and_page)
     Page.set_callback(:update, :before, :update_children_section_id, if: :section_id_changed?)
     Page.set_callback(:update, :before, :archive_children_pages, if: :state_changed?)
     duplicate
+  end
+
+  def footer_pages
+    pages.where(footer: true)
   end
 
   def all_comment_ids
