cb743c9e3714b862286b12be47c615840c618846
Pratibha <pratibhamansinghka@gmail.com>
2016-12-14 14:08:50 +0530

    add user-type for user in analytics

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,6 +4,8 @@ class Admin::UsersController < ApplicationController
   before_filter :load_user, only: [:destroy, :edit, :update]
   before_filter :restrict_current_user, only: [:destroy]
   after_filter :track, only: [:destroy]
+  after_filter :track_user_custom_fields, only: [:update]
+
   def edit
   end
 
@@ -42,6 +44,11 @@ class Admin::UsersController < ApplicationController
   end
 
   private
+
+    def track_user_custom_fields
+      @analytics = Analytics.new(@user)
+      super
+    end
 
     def track
       analytics.track_pro_plan(Plan::PRO) if current_business.is_pro_plan? && @user.destroyed?
