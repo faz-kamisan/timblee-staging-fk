c64cf68edcfa272a3a0eb140f8d9a588b7f5f127
Pratibha <pratibhamansinghka@gmail.com>
2017-01-12 13:32:00 +0530

    Fix tracking company web sessions

--- app/assets/javascripts/intercom.js
+++ app/assets/javascripts/intercom.js
@@ -3,4 +3,10 @@ $(function() {
     email: $('.get-in-touch').data('user-email') || $('#user_email').val(),
     app_id: INTERCOM_APP_ID,
     custom_launcher_selector: '.get-in-touch'};
+
+  if($('.business-info').data('business-id')){
+    window.intercomSettings['company']= {
+      id: $('.business-info').data('business-id')
+    }
+  }
 });
