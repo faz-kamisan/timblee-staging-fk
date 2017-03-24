91333a48a95f3f080a254691abcbbd0720a7f569
Pratibha <pratibhamansinghka@gmail.com>
2017-03-24 12:27:27 +0530

    Revert "temp"
    This reverts commit e5019b9744addcfb1ffb3a70ed41e0da8066e47e.

--- db/migrate/20170315132039_change_businesses.rb
+++
@@ -1,5 +0,0 @@
-class ChangeBusinesses < ActiveRecord::Migration
-  def change
-    change_column :businesses, :trial_days, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170315132039) do
+ActiveRecord::Schema.define(version: 20170228122148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20170315132039) do
     t.integer  "owner_id"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.float    "trial_days"
+    t.integer  "trial_days"
     t.boolean  "has_plan",            default: false
     t.boolean  "is_pro",              default: false
     t.integer  "free_sitemaps_count", default: 1
